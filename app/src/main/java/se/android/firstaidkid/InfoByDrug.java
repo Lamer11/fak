716c0889c8982df07590c52a0ae43dfaf06d4527
Sergey Emelyanov <sergey.emelyanov@ctconsult.ru>
2019-09-11 18:58:33 +0300

    пошло поехало

---
+++ app/src/main/java/se/android/firstaidkid/InfoByDrug.java
@@ -0,0 +1,36 @@
+package se.android.firstaidkid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class InfoByDrug extends AppCompatActivity {
+
+    TextView preparat;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_by_drug);
+
+
+        Bundle arguments = getIntent().getExtras();
+        String name = arguments.getString("key");
+
+        preparat = (TextView) findViewById(R.id.textView13);
+        preparat.setText(name);
+
+    }
+
+
+    public void editDrug(View view){
+
+        // the logic for editing drugs
+
+    }
+}
--- app/src/main/java/se/android/firstaidkid/ListPrepar.java
+++ app/src/main/java/se/android/firstaidkid/ListPrepar.java
@@ -1,22 +1,21 @@
 package se.android.firstaidkid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -31,10 +30,19 @@ public class ListPrepar extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_prepar);
 
+        // получаем элемент TextView
+        selection = (TextView) findViewById(R.id.selection);
+        // получаем элемент ListView
+        countriesList = (ListView) findViewById(R.id.countriesList);
 
+
+
+
+        // создаем и инициализируем базу данных Firebase внутри метода onCreate()
         firebase.collection("apteka").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -44,42 +52,45 @@ public class ListPrepar extends AppCompatActivity {
                         Map<String, Object> docum = document.getData();
                         //вытаскиеваем из карты значение по ключу drug и запихиваем в массив list
                         listDrugs.add((String) docum.get("drug"));
-                    } System.out.println(listDrugs);
+                    } System.out.println("Массив данных = " + listDrugs); // проверка в логах массивчика
+
+
+                    // создаем адаптер (прям в этом во всем это асинхронном. Асинхронное это все для получения данных из базы данных)
+                    ArrayAdapter<String> adapter = new ArrayAdapter(getApplicationContext(),
+                            android.R.layout.simple_list_item_1, listDrugs);
+                    // устанавливаем для списка адаптер
+                    countriesList.setAdapter(adapter);
+                    // добавляем для списка слушатель
+                    countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+                        @Override
+                        //тык на каждый элемент
+                        public void onItemClick(AdapterView<?> parent, View v, int position, long id)
+                        {
+                            // по позиции получаем выбранный элемент
+                            String selectedItem = listDrugs.get(position);
+                            // установка текста элемента TextView
+                            selection.setText(selectedItem);
+
+                            // создание объекта Intent для запуска InfoByDrug
+                            Intent intent = new Intent(getApplicationContext(), InfoByDrug.class);
+                            // передача объекта с ключом "key" и значением selectedItem
+                            intent.putExtra("key", selectedItem);
+                            //System.out.println("Интент = " + selectedItem);
+                            startActivity(intent);
+
+                        }
+                    });
+
+
                 } else {
-                    //gggggggg
+                    //Если что не так, то показываем мессадж тостом
+                    Toast t = Toast.makeText(getApplicationContext(), "Что-то посло не так", Toast.LENGTH_SHORT);
+                    t.setGravity(Gravity.CENTER, 0,0);
+                    t.show();
                 }
             }
         });
 
-
-
-        // получаем элемент TextView
-        selection = (TextView) findViewById(R.id.selection);
-        // получаем элемент ListView
-        countriesList = (ListView) findViewById(R.id.countriesList);
-
-
-
-    }
-
-    public void updateData(View view){
-
-        // создаем адаптер
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, listDrugs);
-        // устанавливаем для списка адаптер
-        countriesList.setAdapter(adapter);
-        // добвляем для списка слушатель
-        countriesList.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id)
-            {
-                // по позиции получаем выбранный элемент
-                String selectedItem = listDrugs.get(position);
-                // установка текста элемента TextView
-                selection.setText(selectedItem);
-            }
-        });
 
     }
 
